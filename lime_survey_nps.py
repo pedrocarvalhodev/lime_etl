a6d0751a1610c7bbb876479e219a2234a7064b6b
pedrocarvalhodev <pedrocarvalhodev@gmail.com>
2018-06-19 20:07:33 -0300

    initial commit

---
+++ lime_survey_nps.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+
+from limesurvey import Api
+import base64
+import pandas
+import json
+import datetime
+import re
+from boto3.session import Session
+from io import StringIO
+import pandas_redshift as pr
+
+from configparser import ConfigParser
+config = ConfigParser()
+config.read( '/home/pgarcia/repos/prod/lime_etl/.config.ini' )
+
+s3_credentials_AWS_ACCESS_KEY=config['AWS_S3']['AWS_ACCESS_KEY']
+s3_credentials_AWS_SECRET_KEY=config['AWS_S3']['AWS_SECRET_KEY']
+s3_credentials_BUCKET=config['AWS_S3']['BUCKET']
+
+redshift_credentials_dbname=config['AWS_REDSHIFT']['DBNAME']
+redshift_credentials_host=config['AWS_REDSHIFT']['HOST']
+redshift_credentials_port=config['AWS_REDSHIFT']['PORT']
+redshift_credentials_user=config['AWS_REDSHIFT']['USER']
+redshift_credentials_password=config['AWS_REDSHIFT']['PASSWORD']
+
+
+user=config['LIME_CONFIG']['LIME_USER']
+key=config['LIME_CONFIG']['LIME_KEY']
+url=config['LIME_CONFIG']['LIME_API_URL']
+sid=config['LIME_CONFIG']['LIME_SID_ORDER']
+token=config['LIME_CONFIG']['LIME_TOKEN_BASE']
+
+
+print(s3_credentials_AWS_ACCESS_KEY)
+print(s3_credentials_AWS_SECRET_KEY)
+print(s3_credentials_BUCKET)
+print(redshift_credentials_dbname)
+print(redshift_credentials_host)
+print(redshift_credentials_port)
+print(redshift_credentials_user)
+print(redshift_credentials_password)
+print(user)
+print(key)
+print(url)
+print(token)
+# Authentication
+#user = config.LIME_USER
+#key = config.LIME_KEY
+#url = config.LIME_API_URL
+DATE_NOW = datetime.datetime.now().strftime('%Y%m%d')
+
+# Build the API
+lime = Api(url, user, key)
+
+export_res_token = lime.export_responses(sid=sid, status='all', heading='code', response='short', fields='')
+OUTPUT_PATH = "/home/pgarcia/Downloads/LimeSurveyData/lime_export_{s}_{d}.txt".format(s=sid, d=DATE_NOW)
+
+with open(OUTPUT_PATH, 'w') as outfile:
+  json.dump(export_res_token, outfile)
+
+d = pandas.read_json(OUTPUT_PATH)
+df = pandas.DataFrame()
+for row in d.index:
+    data = pandas.DataFrame(d.loc[row].loc['responses']).transpose().reset_index(drop=False)
+    df = df.append(data,ignore_index=True)
+
+df = df[['index', 'submitdate', 'lastpage', 'startlanguage', 'startdate', 'datestamp', 'q01', 'q03', 'q06']]
+
+
+cols = {'index'         : 'id_answer',        # ,'ID da resposta'
+        'submitdate'    : 'date_sent',        #'Data de envio'
+        'lastpage'      : 'last_page',        # 'Última página'
+        'startlanguage' : 'language',         #'Idioma inicial'
+        'startdate'     : 'start_date',       #'Data de início'
+        'datestamp'     : 'last_action_date', # 'Data da última ação'
+        'q01'           : 'nps',              #'NPS Score'
+        'q03'           : 'email',            #'E-MAIL'
+        'q06'           : 'cohort'            #, 'Cohort'
+        }
+
+df.rename(index=str, columns=cols, inplace=True)
+df["updated_ts"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+df = df.dropna(axis=0, how='any', thresh=None, subset=['email','nps'], inplace=False)
+#df["nps"] = df.nps.apply(lambda x : float(x.replace("A","")))
+
+df["nps"] = df.nps.apply(lambda x : float(re.sub("A|N", "", x)))
+
+#df.to_csv("/home/pgarcia/Downloads/LimeSurveyData/results-survey615478pppppp.csv")
+
+BUCKET_FOLDER = "limesurvey"
+SCHEMA = "manual_data_sources"
+
+S3_FULL_FILE_NAME = "limesurvey/limesurvey_nps_{d}.csv".format(d=DATE_NOW)
+REDSHIFT_TABLE_NAME = "manual_data_sources.limesurvey_nps_survey"
+
+print('5. Export to csv buffer')
+csv_buffer = StringIO()
+df.to_csv(csv_buffer, index=False)
+
+print('6. Upload to S3')
+session = Session(aws_access_key_id     = s3_credentials_AWS_ACCESS_KEY,
+                  aws_secret_access_key = s3_credentials_AWS_SECRET_KEY)
+
+FILE_NAME = BUCKET_FOLDER + "/" + REDSHIFT_TABLE_NAME + "_" + DATE_NOW + ".csv"
+s3 = session.resource('s3')
+bucket = s3.Bucket(s3_credentials_BUCKET)
+s3.Object(s3_credentials_BUCKET, FILE_NAME).put(Body=csv_buffer.getvalue())
+
+
+print('3. Connect to S3')
+pr.connect_to_s3(aws_access_key_id     = s3_credentials_AWS_ACCESS_KEY,
+                 aws_secret_access_key = s3_credentials_AWS_SECRET_KEY,
+                 bucket                = s3_credentials_BUCKET,
+                 subdirectory          = BUCKET_FOLDER)
+
+print('4. Connect to Redshift')
+pr.connect_to_redshift(dbname    = redshift_credentials_dbname,
+                        host     = redshift_credentials_host,
+                        port     = redshift_credentials_port,
+                        user     = redshift_credentials_user,
+                        password = redshift_credentials_password)
+
+
+def delete_from_date(date):
+    """ Delete data from table """
+    query = "DELETE FROM {table} WHERE start_date >= '{datef}'".format(table=REDSHIFT_TABLE_NAME, datef=date)
+    print("PRINT SQL STATEMENT: ",query)
+    pr.exec_commit(query)
+    return None
+
+delete_from_date(date='2018-01-01')
+
+
+print('5. Create table')
+pr.exec_commit("""
+CREATE TABLE IF NOT EXISTS
+    {table}
+         (id_answer varchar(256),
+          date_sent varchar(256),
+          last_page varchar(256),
+          language varchar(256),
+          start_date varchar(256),
+          last_action_date varchar(256),
+          nps varchar(256),
+          email varchar(256),
+          cohort varchar(256),
+          updated_ts varchar(256));""".format(table=REDSHIFT_TABLE_NAME))
+
+
+print('6. Upload to Redshift')
+pr.exec_commit("""
+  COPY {table}
+  FROM 's3://amaro-bi/{filepath}'
+  ACCEPTINVCHARS 
+  delimiter ','
+  ignoreheader 1
+  csv quote as '"'
+  dateformat 'auto'
+  timeformat 'auto'
+  region 'sa-east-1'
+  access_key_id '{access_key}'
+  secret_access_key '{secret_key}';
+""".format(table = REDSHIFT_TABLE_NAME,
+           filepath = FILE_NAME,
+           access_key = s3_credentials_AWS_ACCESS_KEY,
+           secret_key = s3_credentials_AWS_SECRET_KEY))
+
+print('Finished processing')
+
+
+pr.close_up_shop()
---
+++ lime_survey_orders_shipped.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+
+from limesurvey import Api
+import base64
+import pandas
+import json
+import datetime
+
+from boto3.session import Session
+from io import StringIO
+import pandas_redshift as pr
+
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read( '/home/pgarcia/repos/prod/lime_etl/.config.ini' )
+
+s3_credentials_AWS_ACCESS_KEY=config['AWS_S3']['AWS_ACCESS_KEY']
+s3_credentials_AWS_SECRET_KEY=config['AWS_S3']['AWS_SECRET_KEY']
+s3_credentials_BUCKET=config['AWS_S3']['BUCKET']
+
+redshift_credentials_dbname=config['AWS_REDSHIFT']['DBNAME']
+redshift_credentials_host=config['AWS_REDSHIFT']['HOST']
+redshift_credentials_port=config['AWS_REDSHIFT']['PORT']
+redshift_credentials_user=config['AWS_REDSHIFT']['USER']
+redshift_credentials_password=config['AWS_REDSHIFT']['PASSWORD']
+
+
+user=config['LIME_CONFIG']['LIME_USER']
+key=config['LIME_CONFIG']['LIME_KEY']
+url=config['LIME_CONFIG']['LIME_API_URL']
+sid=config['LIME_CONFIG']['LIME_SID_ORDER']
+token=config['LIME_CONFIG']['LIME_TOKEN_BASE']
+
+DATE_NOW = datetime.datetime.now().strftime('%Y%m%d')
+
+# Build the API
+lime = Api(url, user, key)
+export_res_token = lime.export_responses(sid=sid, status='all', heading='code', response='short', fields='')
+OUTPUT_PATH = "/home/pgarcia/Downloads/LimeSurveyData/lime_export_{s}_{d}.txt".format(s=sid, d=DATE_NOW)
+
+with open(OUTPUT_PATH, 'w') as outfile:
+	json.dump(export_res_token, outfile)
+
+
+df = pandas.read_json(OUTPUT_PATH)
+ds = pandas.DataFrame()
+for row in df.index:
+    data = pandas.DataFrame(df.loc[row].loc['responses']).transpose().reset_index(drop=False)
+    ds = ds.append(data,ignore_index=True)
+
+
+req_cols = ['id', 'datestamp', 'q01', 'q03', 'q06']
+ds = ds[req_cols]
+ds = ds.dropna()
+
+
+cols = {'id'        : 'id_answer',
+        'datestamp' : 'date_sent',
+        'q01'       : 'grade',
+        'q03'       : 'email',
+        'q06'       : 'order_number'}
+
+ds.rename(index=str, columns=cols, inplace=True)
+ds["updated_ts"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+ds = ds.sort_values(by=['order_number', 'date_sent'], ascending=[True, True])
+ds = ds.drop_duplicates(subset='order_number', keep='last').reset_index(drop=True)
+
+
+ds = ds[ds.grade.isin(['A1', 'A2', 'A3', 'A4', 'A5'])].copy()
+ds["grade"] = ds.grade.apply(lambda x : float(x.replace("A","")))
+
+# ## AWS
+
+BUCKET_FOLDER = "limesurvey"
+SCHEMA = "manual_data_sources"
+
+S3_FULL_FILE_NAME = "limesurvey/limesurvey_{d}.csv".format(d=DATE_NOW)
+REDSHIFT_TABLE_NAME = "manual_data_sources.limesurvey_pedido_entregue_survey"
+
+print('5. Export to csv buffer')
+csv_buffer = StringIO()
+ds.to_csv(csv_buffer, index=False)
+
+print('6. Upload to S3')
+session = Session(aws_access_key_id     = s3_credentials_AWS_ACCESS_KEY,
+                  aws_secret_access_key = s3_credentials_AWS_SECRET_KEY)
+
+FILE_NAME = BUCKET_FOLDER + "/" + REDSHIFT_TABLE_NAME + "_" + DATE_NOW + ".csv"
+s3 = session.resource('s3')
+bucket = s3.Bucket(s3_credentials_BUCKET)
+s3.Object(s3_credentials_BUCKET, FILE_NAME).put(Body=csv_buffer.getvalue())
+
+
+print('3. Connect to S3')
+pr.connect_to_s3(aws_access_key_id     = s3_credentials_AWS_ACCESS_KEY,
+                 aws_secret_access_key = s3_credentials_AWS_SECRET_KEY,
+                 bucket                = s3_credentials_BUCKET,
+                 subdirectory          = BUCKET_FOLDER)
+
+print('4. Connect to Redshift')
+pr.connect_to_redshift(dbname    = redshift_credentials_dbname,
+                        host     = redshift_credentials_host,
+                        port     = redshift_credentials_port,
+                        user     = redshift_credentials_user,
+                        password = redshift_credentials_password)
+
+def delete_from_date(date):
+	""" Delete data from table """
+	query = "DELETE FROM manual_data_sources.limesurvey_pedido_entregue_survey WHERE date_sent >= '{datef}'"
+	sql_statm = query.format(datef=date)
+	print("PRINT SQL STATEMENT: ",sql_statm)
+	pr.exec_commit(sql_statm)
+	return None
+
+delete_from_date(date='2018-01-01')
+
+
+
+print('5. Create table')
+pr.exec_commit("""
+CREATE TABLE IF NOT EXISTS
+    {fn}
+         (id_answer varchar(256),
+         date_sent varchar(256),
+         grade varchar(256),
+         email varchar(256),
+         order_number varchar(256),
+         updated_ts varchar(256));""".format(fn=REDSHIFT_TABLE_NAME))
+
+
+print('6. Upload to Redshift')
+pr.exec_commit("""
+  COPY {fn}
+  FROM 's3://amaro-bi/{filepath}'
+  ACCEPTINVCHARS 
+  delimiter ','
+  ignoreheader 1
+  csv quote as '"'
+  dateformat 'auto'
+  timeformat 'auto'
+  region 'sa-east-1'
+  access_key_id '{access_key}'
+  secret_access_key '{secret_key}';
+""".format(fn = REDSHIFT_TABLE_NAME,
+           filepath = FILE_NAME,
+           access_key = s3_credentials_AWS_ACCESS_KEY,
+           secret_key = s3_credentials_AWS_SECRET_KEY))
+
+print('Finished processing')
+
+pr.close_up_shop()
+
+print("Closed connection")
